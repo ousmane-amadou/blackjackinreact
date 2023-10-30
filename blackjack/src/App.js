1b861d9b2934df0707934a76006dbda710d73262
unknown <ousmane.amadou89@gmail.com>
2023-10-30 14:10:59 -0400

    .splice() returns an array, first return value is only needed to get the card removed from deck

--- blackjack/src/App.js
+++ blackjack/src/App.js
@@ -1,20 +1,37 @@
 // import './App.css';
-import {createDeck} from './deck';
 
 function App() {
-  const gameDeck = createDeck()
+  const gameDeck = require('./deck.json')
 
   const drawCard = () => { 
     const cardIndex = Math.floor(Math.random()*gameDeck.length)
-    return gameDeck.splice(cardIndex, 1)
+    return gameDeck.splice(cardIndex, 1)[0]
   } 
   const playerHand = [drawCard(), drawCard()]; 
   const dealerHand = [drawCard(), drawCard()];
-  console.log(playerHand, dealerHand, gameDeck.length)
+  const cardInPlay = drawCard();
+
+  const handleHit = (e) => {
+    e.preventDefault()
+    playerHand.push(drawCard());
+  }
+
+  const handleStay = (e) => {
+    e.preventDefault()
+  }
   
   return (
     <div className="App">
-    
+      <h1>Player 1 Hand </h1>
+      { 
+        playerHand.map((card, index) => (
+            <p key={index}>{card.name}, {card.suit}, {card.value}</p>
+        ))
+      }
+      <button onClick={handleHit}>
+        Hit
+      </button>
+      <button onClick={handleStay}>Test</button>
     </div>
   );
 }
