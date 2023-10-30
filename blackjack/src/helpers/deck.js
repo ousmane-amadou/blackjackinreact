de7e8aa49a909263aee50eb1569b0356f334d5a2
unknown <ousmane.amadou89@gmail.com>
2023-10-30 14:07:22 -0400

    Added logic to generate deck.json

---
+++ blackjack/src/helpers/deck.js
@@ -0,0 +1,45 @@
+// Used to generate deck.json
+const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+
+const values = [
+  { name: '2', value: 2 },
+  { name: '3', value: 3 },
+  { name: '4', value: 4 },
+  { name: '5', value: 5 },
+  { name: '6', value: 6 },
+  { name: '7', value: 7 },
+  { name: '8', value: 8 },
+  { name: '9', value: 9 },
+  { name: '10', value: 10 },
+  { name: 'Jack', value: 10 },
+  { name: 'Queen', value: 10 },
+  { name: 'King', value: 10 },
+  { name: 'Ace', value: 11 }  // Ace's value can also be 1, but this needs to be handled in the game logic.
+];
+
+// Each card is represented by a Javascript object
+export const createDeck = () => {
+  const deck = [];
+  suits.forEach(suit => {
+    values.forEach(card => {
+      deck.push({
+        suit: suit,
+        name: card.name,
+        value: card.value
+      });
+    });
+  });
+  return deck
+}
+const fs = require('fs');
+const Deck = createDeck();
+const jsonString = JSON.stringify(Deck, null, 2);
+
+fs.writeFile('output.json', jsonString, (err) => {
+  if (err) {
+    console.error('Error writing to file:', err);
+  } else {
+    console.log('File has been saved.');
+  }
+});
+
