f345075a3df342c5485a2b0a50a518bc867d2f11
Home <home@Homes-Air.ht.home>
2024-05-30 17:28:35 -0400

    Renamed ace of spades appropriately; Changed how the deck is loaded into the game

--- blackjack/src/Game.js
+++ blackjack/src/Game.js
@@ -1,25 +1,39 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import jsonData from './deck.json'
 
 function MyComponent({ winner }) {
   if (winner == 'dealer') {
-    return <p>Dealer is the winner</p>;
+    return <p>Dealer is the winner!</p>;
   } else if (winner == 'player') {
-    return <p>Player is the winner</p>;
+    return <p>Player is the winner!</p>;
+  } else if (winner == 'draw'){
+    return <p> It is a draw! </p>
   }
 };
 
 function Game() {
-  const gameDeck = require('./deck.json')
+  const gameDeck = JSON.parse(JSON.stringify(jsonData));
+  const [deck, setDeck] = useState(gameDeck);
   const [winner, setWinner] = useState('');
   const [isButtonDisabled, setButtonDisabled] = useState(false);
 
   const drawCard = () => { 
-    const cardIndex = Math.floor(Math.random()*gameDeck.length)
-    return gameDeck.splice(cardIndex, 1)[0]
+    const cardIndex = Math.floor(Math.random()*deck.length)
+    const card = deck.splice(cardIndex, 1)[0]
+  
+    console.log(deck.length, cardIndex)
+    return card
   } 
-  const [playerHand, setPlayerHand] = useState([drawCard(), drawCard()]); 
-  const [dealerHand, setDealerHand] = useState([drawCard()])
+
+  const [playerHand, setPlayerHand] = useState([]); 
+  const [dealerHand, setDealerHand] = useState([])
+
+  useEffect(() => {
+    console.log("init")
+    setPlayerHand([drawCard(), drawCard()]);
+    setDealerHand([drawCard()]);
+  }, []);
 
   const calculateWinner = () => {
     let playerTotal = 0; let dealerTotal = 0;
@@ -37,26 +51,13 @@ function Game() {
       setWinner("draw")
     }
   }
-  // const checkHand = (hand) => {
-  //   let handTotal = 0
-    
-  //   for(let i = 0; i < hand.length; i++) {
-  //     handTotal += hand[i].value
-  //   }
-  
-  //   if (handTotal > 21) {
-  //     setButtonDisabled(true);
-  //     return "LOSS"
-  //   }
-  //   else if (handTotal == 21) {
-  //     setButtonDisabled(true);
-  //     return "WIN"
-  //   }
-  //   else {
-  //     return "PLAYING"
-  //   }
-  // }
-
+  const handleReset = (e) => {
+    console.clear()
+    setDeck(gameDeck)
+    setWinner(''); setButtonDisabled(false);
+    setPlayerHand([drawCard(), drawCard()]); 
+    setDealerHand([drawCard()]);
+  }
   const handleHit = (e) => {
     const newHand = JSON.parse(JSON.stringify(playerHand))
 
@@ -90,9 +91,8 @@ function Game() {
       for(let i = 0; i < newHand.length; i++) {
         handTotal += newHand[i].value
       }
-      
+
       setDealerHand(newHand)
-      console.log(handTotal)
     } 
     while(handTotal < 17);
     
@@ -118,32 +118,35 @@ function Game() {
   return (
     <div className="App">
       <div className="container">
-          <h1>Dealer</h1>
-          <div className='dealer'>
-            { 
-              dealerHand.map((card, index) => (
-                  <img 
-                    key={index}
-                    src={getCardImageURL(card)}/>
-              ))
-            }
-          </div>
-          <h1>Player</h1>
-          <div className='player'>
-            { 
-              playerHand.map((card, index) => (
-                  <img 
-                    key={index}
-                    src={getCardImageURL(card)}/>
-              ))
-            }
-          </div>
-          <div className="play-buttons">
-            <button onClick={handleHit} disabled={isButtonDisabled}> Hit </button>
-            <button onClick={handleStay} disabled={isButtonDisabled}> Stand </button>
-          </div>
-          <div>
-            <MyComponent winner={winner}/>
+          <div className="centered-content">
+            <h1>Dealer</h1>
+            <div className='dealer'>
+              { 
+                dealerHand.map((card, index) => (
+                    <img 
+                      key={index}
+                      src={getCardImageURL(card)}/>
+                ))
+              }
+            </div>
+            <h1>Player</h1>
+            <div className='player'>
+              { 
+                playerHand.map((card, index) => (
+                    <img 
+                      key={index}
+                      src={getCardImageURL(card)}/>
+                ))
+              }
+            </div>
+            <div className="play-buttons">
+              <button onClick={handleHit} disabled={isButtonDisabled}> Hit </button>
+              <button onClick={handleStay} disabled={isButtonDisabled}> Stand </button>
+              <button onClick={handleReset}> Reset </button>
+            </div>
+            <div>
+              <MyComponent winner={winner}/>
+            </div>
           </div>
         </div>
     </div>
